d81f5122ff3f4d2950aea18fb3c4b037b3492a8d
Sebastian Thiel <sthiel@thoughtworks.com>
2019-08-19 12:39:20 +0800

    Implement first version of library generation via CLI

--- mcp/src/cmds/generate.rs
+++ mcp/src/cmds/generate.rs
@@ -1,5 +1,8 @@
 use crate::options::generate::Args;
-use failure::Error;
+use discovery_parser::DiscoveryRestDesc;
+use failure::{format_err, Error, ResultExt};
+use google_rest_api_generator::generate as generate_library;
+use std::fs;
 
 pub fn execute(
     Args {
@@ -7,5 +10,12 @@ pub fn execute(
         output_directory,
     }: Args,
 ) -> Result<(), Error> {
-    unimplemented!()
+    let desc: DiscoveryRestDesc = { serde_json::from_slice(&fs::read(&spec_json_path)?) }
+        .with_context(|_| format_err!("Could read spec file at '{}'", spec_json_path.display()))?;
+
+    // TODO: I vote for making the crate names compatible to the existing ones, and signal breakage via semver
+    let project_name = format!("google_{}_{}", &desc.name, &desc.version);
+    generate_library(&project_name, &desc, output_directory.join("lib"))
+        .map_err(|e| format_err!("{}", e.to_string()))?;
+    Ok(())
 }
