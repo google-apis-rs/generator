ea952a867be62700dcc4a03246cabeea08151ae8
Sebastian Thiel <sthiel@thoughtworks.com>
2019-08-19 12:56:50 +0800

    Provide information about RUSTFMT= to speed up code generation

--- mcp/src/options/generate.rs
+++ mcp/src/options/generate.rs
@@ -3,6 +3,10 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+/// Generate libraries and command-line interface for said libraries based on Google API specifications.
+///
+/// The output will always be formatted using rustfmt. You can set the RUSTFMT environment variable to an
+/// empty value to prevent formatting, which can safe some time during generation.
 pub struct Args {
     /// The Google API specification as downloaded from the discovery service
     #[structopt(parse(from_os_str))]
