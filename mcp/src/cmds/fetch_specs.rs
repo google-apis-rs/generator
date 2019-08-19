7e5cd4ea569358f77534df9f2b39d73d98de3805
Sebastian Thiel <sthiel@thoughtworks.com>
2019-08-19 12:12:50 +0800

    simplify MCP

--- mcp/src/cmds/fetch_specs.rs
+++ mcp/src/cmds/fetch_specs.rs
@@ -3,49 +3,11 @@ use discovery_parser::{
     generated::{DiscoveryApisV1, Item},
     DiscoveryRestDesc, RestDescOrErr,
 };
-use failure::{bail, format_err, Error, ResultExt};
+use failure::{format_err, Error, ResultExt};
 use failure_tools::print_causes;
 use log::{error, info};
 use rayon::prelude::*;
-use std::{convert::TryFrom, convert::TryInto, fs, path::Path, time::Instant};
-
-#[derive(Debug, PartialEq, Eq)]
-struct Id {
-    name: String,
-    version: String,
-}
-
-impl TryFrom<&str> for Id {
-    type Error = Error;
-
-    fn try_from(s: &str) -> Result<Id, Error> {
-        let mut tokens = s.rsplit(':');
-        match (tokens.next(), tokens.next()) {
-            (Some(v), Some(n)) => Ok(Id {
-                name: n.into(),
-                version: v.into(),
-            }),
-            _ => bail!("Could not parse '{}' as id like 'name:version'", s),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Api {
-    id: Id,
-    url: String,
-}
-
-impl<'a> TryFrom<&'a Item> for Api {
-    type Error = Error;
-
-    fn try_from(value: &Item) -> Result<Api, Error> {
-        Ok(Api {
-            id: value.id.as_str().try_into()?,
-            url: value.discovery_rest_url.clone(),
-        })
-    }
-}
+use std::{fs, path::Path, time::Instant};
 
 fn log_error_and_continue<T, E: Into<Error>>(r: Result<T, E>) -> Option<T> {
     match r {
@@ -82,10 +44,10 @@ fn logged_write<P: AsRef<Path>, C: AsRef<[u8]>>(
 }
 
 fn write_artifacts<'a>(
-    (api, spec): (Api, DiscoveryRestDesc),
+    spec: DiscoveryRestDesc,
     output_dir: &Path,
-) -> Result<Api, Error> {
-    let output_dir = output_dir.join(&api.id.name).join(&api.id.version);
+) -> Result<DiscoveryRestDesc, Error> {
+    let output_dir = output_dir.join(&spec.name).join(&spec.version);
     fs::create_dir_all(&output_dir).with_context(|_| {
         format_err!(
             "Could not create artifact output directory at '{}'",
@@ -101,24 +63,23 @@ fn write_artifacts<'a>(
         serde_json::to_string_pretty(&spec)?.as_bytes(),
         "spec",
     )?;
-    Ok(api)
+    Ok(spec)
 }
 
-fn fetch_spec(api: Api) -> Result<(Api, DiscoveryRestDesc), Error> {
-    reqwest::get(&api.url)
-        .with_context(|_| format_err!("Could not fetch spec from '{}'", api.url))
+fn fetch_spec(api: &Item) -> Result<DiscoveryRestDesc, Error> {
+    reqwest::get(&api.discovery_rest_url)
+        .with_context(|_| format_err!("Could not fetch spec from '{}'", api.discovery_rest_url))
         .map_err(Error::from)
         .and_then(|mut r: reqwest::Response| {
-            let res: RestDescOrErr = r
-                .json()
-                .with_context(|_| format_err!("Could not deserialize spec at '{}'", api.url))?;
+            let res: RestDescOrErr = r.json().with_context(|_| {
+                format_err!("Could not deserialize spec at '{}'", api.discovery_rest_url)
+            })?;
             match res {
                 RestDescOrErr::RestDesc(v) => Ok(v),
                 RestDescOrErr::Err(err) => Err(format_err!("{:?}", err.error)),
             }
         })
-        .with_context(|_| format_err!("Error fetching spec from '{}'", api.url))
-        .map(|spec: DiscoveryRestDesc| (api, spec))
+        .with_context(|_| format_err!("Error fetching spec from '{}'", api.discovery_rest_url))
         .map_err(Into::into)
 }
 
@@ -139,42 +100,15 @@ pub fn execute(
     let time = Instant::now();
     apis.items
         .par_iter()
-        .map(Api::try_from)
-        .filter_map(log_error_and_continue)
         .map(fetch_spec)
         .filter_map(log_error_and_continue)
         .map(|v| write_artifacts(v, &output_directory))
         .filter_map(log_error_and_continue)
-        .for_each(|api| info!("Successfully processed ${:?}", api));
+        .for_each(|api| info!("Successfully processed {}:{}", api.name, api.version));
     info!(
-        "Processed {} specs in {}s",
+        "Fetched {} specs in {}s",
         apis.items.len(),
         time.elapsed().as_secs()
     );
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    mod id {
-        use super::*;
-
-        #[test]
-        fn valid_ids_work() {
-            assert_eq!(
-                Id {
-                    name: "name".into(),
-                    version: "version".into()
-                },
-                Id::try_from("name:version").unwrap()
-            )
-        }
-
-        #[test]
-        fn invalid_ids_yield_an_error() {
-            assert!(Id::try_from("nameversion").is_err())
-        }
-    }
 }
