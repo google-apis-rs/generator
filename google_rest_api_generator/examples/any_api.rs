b55c679bebd10be192cd9f106ab55bd2fb4b105f
Glenn Griffin <ggriffiniii@gmail.com>
2019-08-20 11:17:51 -0700

    any_api now accepts command line arguments to determine which api to generate.

--- google_rest_api_generator/examples/any_api.rs
+++ google_rest_api_generator/examples/any_api.rs
@@ -1,7 +1,22 @@
 use discovery_parser::DiscoveryRestDesc;
 use google_rest_api_generator as generator;
 use std::error::Error;
-use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "any_api",
+    about = "Generate Rust API bindings for a Google Rest API"
+)]
+struct Opt {
+    #[structopt(long = "name")]
+    name: String,
+    #[structopt(long = "version")]
+    version: String,
+    #[structopt(long = "output_dir", default_value = "/tmp", parse(from_os_str))]
+    output_dir: PathBuf,
+}
 
 /// Alter the URL to generate output for a different API.
 /// Otherwise, prefer using the machinery in https://github.com/google-apis-rs/generated to
@@ -10,11 +25,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     ::env_logger::builder()
         .default_format_timestamp_nanos(true)
         .init();
-    let desc: DiscoveryRestDesc =
-        reqwest::get("https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest")?
-            .error_for_status()?
-            .json()?;
+    let opt = Opt::from_args();
+    let desc: DiscoveryRestDesc = reqwest::get(&format!(
+        "https://www.googleapis.com/discovery/v1/apis/{}/{}/rest",
+        &opt.name, &opt.version
+    ))?
+    .error_for_status()?
+    .json()?;
     let project_name = format!("google_{}_{}", &desc.name, &desc.version);
-    generator::generate(&project_name, &desc, Path::new("/tmp").join(&project_name))?;
+    let project_dir = opt.output_dir.join(&project_name);
+    println!("Writing to {:?}", &project_dir);
+    generator::generate(&project_name, &desc, &project_dir)?;
     Ok(())
 }
