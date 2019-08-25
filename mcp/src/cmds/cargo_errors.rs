f853b257c31def4197a11568a7f8b00fe0373b8a
Sebastian Thiel <byronimo@gmail.com>
2019-08-25 17:44:24 +0800

    cargo-errors: This might be working logic

--- mcp/src/cmds/cargo_errors.rs
+++ mcp/src/cmds/cargo_errors.rs
@@ -1,7 +1,7 @@
 use crate::options::cargo_errors::Args;
 use cargo_log_parser::parse_errors;
 use failure::{bail, Error, ResultExt};
-use log::error;
+use log::{error, info};
 use std::{
     io::{self, Read, Write},
     process::{Command, Stdio},
@@ -15,82 +15,108 @@ pub fn execute(
         mut cargo_arguments,
     }: Args,
 ) -> Result<(), Error> {
-    cargo_arguments.push("--manifest-path".into());
-    cargo_arguments.push(cargo_manifest_path.into());
-    let mut cargo = Command::new("cargo")
-        .args(cargo_arguments)
-        .stderr(Stdio::piped())
-        .stdout(Stdio::inherit())
-        .stdin(Stdio::null())
-        .spawn()
-        .with_context(|_| "failed to launch cargo")?;
+    let mut excludes = Vec::new();
+    let mut last_excludes_len = 0;
 
-    let mut input = Vec::new();
-    let mut print_from = 0_usize;
     loop {
-        let written_bytes = io::stderr().write(&input[print_from..])?;
-        print_from = written_bytes;
+        cargo_arguments.push("--manifest-path".into());
+        cargo_arguments.push(cargo_manifest_path.clone().into());
+        cargo_arguments.extend(
+            excludes
+                .iter()
+                .map(|crate_name| format!("--exclude={}", crate_name).into()),
+        );
+        let mut cargo = Command::new("cargo")
+            .args(&cargo_arguments)
+            .stderr(Stdio::piped())
+            .stdout(Stdio::inherit())
+            .stdin(Stdio::null())
+            .spawn()
+            .with_context(|_| "failed to launch cargo")?;
 
-        let to_read = match parse_errors(&input).map(|(i, r)| (i.len(), r)) {
-            Ok((input_left_len, parsed)) => {
-                dbg!(parsed);
-                let input_len = input.len();
-                input = input.into_iter().skip(input_len - input_left_len).collect();
-                print_from = 0;
-                128
-            }
-            Err(nom::Err::Incomplete(needed)) => {
-                match needed {
-                    nom::Needed::Unknown => 1, // read one byte
-                    nom::Needed::Size(len) => len,
+        let mut input = Vec::new();
+        let mut print_from = 0_usize;
+        loop {
+            let written_bytes = io::stderr().write(&input[print_from..])?;
+            print_from = written_bytes;
+
+            let to_read = match parse_errors(&input).map(|(i, r)| (i.len(), r)) {
+                Ok((input_left_len, parsed)) => {
+                    dbg!(&parsed);
+                    let input_len = input.len();
+                    input = input.into_iter().skip(input_len - input_left_len).collect();
+                    print_from = 0;
+                    excludes.extend(parsed.into_iter().map(|c| c.name));
+                    128
                 }
-            }
-            Err(nom::Err::Failure(_e)) | Err(nom::Err::Error(_e)) => {
-                bail!("TODO: proper error conversion if parsing really fails")
-            }
-        };
+                Err(nom::Err::Incomplete(needed)) => {
+                    match needed {
+                        nom::Needed::Unknown => 1, // read one byte
+                        nom::Needed::Size(len) => len,
+                    }
+                }
+                Err(nom::Err::Failure(_e)) | Err(nom::Err::Error(_e)) => {
+                    bail!("TODO: proper error conversion if parsing really fails")
+                }
+            };
 
-        if let Some(_) = cargo.try_wait()? {
-            break;
+            if let Some(_) = cargo.try_wait()? {
+                break;
+            }
+
+            if let Err(e) = cargo
+                .stderr
+                .as_mut()
+                .expect("cargo_output is set")
+                .take(to_read as u64)
+                .read_to_end(&mut input)
+            {
+                error!("Failed to read cargo output: {}", e);
+                break;
+            }
         }
 
-        if let Err(e) = cargo
+        cargo
             .stderr
             .as_mut()
             .expect("cargo_output is set")
-            .take(to_read as u64)
-            .read_to_end(&mut input)
-        {
-            error!("Failed to read cargo output: {}", e);
-            break;
-        }
-    }
+            .read_to_end(&mut input)?;
 
-    cargo
-        .stderr
-        .as_mut()
-        .expect("cargo_output is set")
-        .read_to_end(&mut input)?;
+        io::stderr().write(&input[print_from..])?;
 
-    match parse_errors(&input) {
-        Ok(parsed) => {
-            dbg!(parsed);
+        match parse_errors(&input) {
+            Ok((_, parsed)) => {
+                dbg!(&parsed);
+                excludes.extend(parsed.into_iter().map(|c| c.name));
+            }
+            Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => {
+                error!("Ignoring parse error after cargo ended: {:?}", e.1);
+            }
+            Err(nom::Err::Incomplete(_)) => panic!(
+                "Could not parse remaining input: {:?}",
+                std::str::from_utf8(&input)
+            ),
+        };
+
+        let workspace_cargo_status = cargo.try_wait()?.expect("cargo ended");
+
+        if workspace_cargo_status.success() {
+            info!("Cargo finished successfully.");
+            if !excludes.is_empty() {
+                info!(
+                    "Recorded errors for the following workspace members: {:?}",
+                    excludes
+                );
+            }
             return Ok(());
+        } else {
+            if last_excludes_len == excludes.len() {
+                bail!(
+                    "cargo seems to fail permanently and makes no progress: {:?}",
+                    workspace_cargo_status
+                );
+            }
+            last_excludes_len = excludes.len();
         }
-        Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => {
-            error!("Ignoring parse error after cargo ended: {:?}", e.1);
-        }
-        Err(nom::Err::Incomplete(_)) => panic!(
-            "Could not parse remaining input: {:?}",
-            std::str::from_utf8(&input)
-        ),
-    };
-
-    let status = cargo.try_wait()?.expect("cargo ended");
-
-    if status.success() {
-        Ok(())
-    } else {
-        bail!("cargo exited with error: {:?}", status)
     }
 }
